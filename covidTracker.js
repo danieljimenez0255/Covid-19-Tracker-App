9323086a5a31cfe992f3f410cc0d30272db93d6b
danieljimenez0255 <42664403+danieljimenez0255@users.noreply.github.com>
2020-05-15 21:48:56 -0700

    Added the main files of the app

---
+++ covidTracker.js
@@ -0,0 +1,10 @@
+// alert("hello")
+
+//intializies the map
+var map;
+function initMap() {
+  map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: -34.397, lng: 150.644 },
+    zoom: 8,
+  });
+}
